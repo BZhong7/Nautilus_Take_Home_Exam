f70da1cf5c7f1aed06bcd10b778ed19c2ee7641b
BZhong7 <zhong.brandon26@gmail.com>
2021-05-19 17:39:57 -0400

    Initial commit

---
+++ simplfy_if_else.py
@@ -0,0 +1,21 @@
+# Refactor the code found in NautilusCodingExercise.pdf
+# in order to make it cleaner, simpler, and easier to maintain
+
+def f():
+    if !A:
+        s()
+        return False
+    z()
+    if !B:
+        t()
+        return False
+    y()
+    if !C:
+        u()
+        return False
+    x()
+    if !D:
+        v()
+        return False
+    w()
+    return True
